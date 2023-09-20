5c37f045673267eac7d99b492de5295ca9b95d6b
Alex Veden <i@alexveden.com>
2023-09-20 09:29:33 +0400

    isort fix

--- asyncfix/protocol/__init__.py
+++ asyncfix/protocol/__init__.py
@@ -1,4 +1,4 @@
-from asyncfix.protocol.fixtags import FTag
-from asyncfix.protocol.msgtype import FMsgType
+from .fixtags import FTag
+from .msgtype import FMsgType
 from asyncfix.protocol.protocol_base import FIXProtocolBase
 from asyncfix.protocol.protocol_fix44 import FIXProtocol44
