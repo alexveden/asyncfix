04c3fd7f8442c61be7358bd042fa881caac95c7a
Alex Veden <i@alexveden.com>
2023-10-14 12:43:33 +0400

    WIP protocol docs

--- asyncfix/protocol/protocol_base.py
+++ asyncfix/protocol/protocol_base.py
@@ -1,7 +1,9 @@
+"""Base FIX protocol."""
 from asyncfix import FMsg, FTag
 
 
 class FIXProtocolBase(object):
+    """Generic FIX protocol."""
     beginstring: str = "FIXProtocolBase"
     repeating_groups: dict[str, list[str]] = {}
     session_message_types: set = set()
