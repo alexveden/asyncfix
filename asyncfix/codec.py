a47a3f379966f25a9e8f60b315552684cc129ab4
Alex Veden <i@alexveden.com>
2023-10-14 10:51:54 +0400

    codec docstrings

--- asyncfix/codec.py
+++ asyncfix/codec.py
@@ -1,3 +1,4 @@
+"""FIX Message encoding / decoding module."""
 import logging
 from datetime import datetime
 
@@ -8,7 +9,7 @@ from asyncfix.protocol import FIXProtocolBase
 from asyncfix.session import FIXSession
 
 
-class RepeatingGroupContext(FIXContainer):
+class _RepeatingGroupContext(FIXContainer):
     def __init__(self, tag, repeating_group_tags, parent):
         self.tag = tag
         self.repeating_group_tags = repeating_group_tags
@@ -17,12 +18,25 @@ class RepeatingGroupContext(FIXContainer):
 
 
 class Codec(object):
+    """Encoding / decoding engine.
+
+    Attributes:
+        protocol: FIX protocol
+        SOH: encoded message separator
+    """
+
     def __init__(self, protocol: FIXProtocolBase):
+        """Codec init.
+
+        Args:
+            protocol: FIX protocol used in encoding/decoding
+        """
         self.protocol: FIXProtocolBase = protocol
         self.SOH = "\x01"
 
     @staticmethod
-    def current_datetime():
+    def current_datetime() -> str:
+        """FIX complaint date-time string (UTC now)."""
         return datetime.utcnow().strftime("%Y%m%d-%H:%M:%S.%f")[:-3]
 
     def _addTag(self, body, t, msg: FIXMessage):
@@ -36,8 +50,24 @@ class Codec(object):
             body.append("%s=%s" % (t, msg[t]))
 
     def encode(
-        self, msg: FIXMessage, session: FIXSession, raw_seq_num: bool = False
+        self,
+        msg: FIXMessage,
+        session: FIXSession,
+        raw_seq_num: bool = False,
     ) -> str:
+        """Encodes FIXMessage into serialized message.
+
+        Args:
+            msg: generic FIXMessage
+            session: current session (for seq num)
+            raw_seq_num: if True - uses MsgSeqNum from `msg`
+
+        Returns:
+            encoded message (string)
+
+        Raises:
+            EncodingError: when failed MsgSeqNum conditions for some types of messages
+        """
         # Create body
         body = []
 
@@ -104,8 +134,20 @@ class Codec(object):
         return fixmsg + SEP
 
     def decode(
-        self, rawmsg: bytes, silent: bool = True
+        self,
+        rawmsg: bytes,
+        silent: bool = True,
     ) -> tuple[FIXMessage | None, int, bytes | None]:
+        """Decodes message from socket.
+
+        Args:
+            rawmsg: message bytes
+            silent: no errors raised, returns non
+
+        Returns:
+            if OK - (FIXMessage, bytes_processed, valid_raw_msg_bytes)
+            if ERR - (None, n_bytes_skip, None)
+        """
         valid_idx = rawmsg.find(b"8=FIX.")
         if valid_idx == -1:
             assert silent, "no fix header"
@@ -199,7 +241,7 @@ class Codec(object):
             # found the start of a repeating group
             if tag in repeating_group_tags:
                 # i.e. we are already in a repeating group
-                if type(current_context) is RepeatingGroupContext:
+                if type(current_context) is _RepeatingGroupContext:
                     while (
                         repeating_groups
                         and tag not in current_context.repeating_group_tags
@@ -211,7 +253,7 @@ class Codec(object):
                         # pop the completed group off the stack
                         del repeating_groups[-1]
 
-                ctx = RepeatingGroupContext(
+                ctx = _RepeatingGroupContext(
                     tag, repeating_group_tags[tag], current_context
                 )
                 repeating_groups.append(ctx)
@@ -235,7 +277,7 @@ class Codec(object):
                     current_context.parent.add_group(
                         current_context.tag, current_context
                     )
-                    ctx = RepeatingGroupContext(
+                    ctx = _RepeatingGroupContext(
                         current_context.tag,
                         current_context.repeating_group_tags,
                         current_context.parent,
